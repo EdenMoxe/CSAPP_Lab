8d9d54c1b388910b4326b0cf1ced38d678ccb383
EdenMoxe <2436444815@qq.com>
2024-08-13 08:01:54 -0700

    origin

---
+++ install.sh
@@ -0,0 +1,7 @@
+labs=( "datalab" "bomblab" "attacklab" "archlab" "cachelab" "shlab" "malloclab" "proxylab" )
+i=1
+for dir in ${labs[*]} ;do
+    lab="lab"$i"$dir"
+    wget https://gitee.com/lin-xi-269/csapplab/raw/origin/$lab/install.sh -O install$lab.sh&& bash install$lab.sh && rm install$lab.sh
+    i=$((i+1))
+done
